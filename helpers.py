1168ec65b36c4e67ed6c20cbbe389c8268299c6d
OJH3KOR <shivhariganesh.korde@bosch.com>
2025-05-15 13:07:26 +0530

    txt error fix2

--- helpers.py
+++ helpers.py
@@ -40,25 +40,25 @@ def load_testbench_data(file):
     elif file_name.endswith(".dat") or file_name.endswith(".mf4"):
         try:
             mdf = asammdf.MDF(BytesIO(file.read()))
-            return mdf.to_dataframe()
+            return None, mdf.to_dataframe()
         except Exception:
-            return None
+            return None, None
 
     elif file_name.endswith(".tdms"):
         try:
             tdms_file = nptdms.TdmsFile(BytesIO(file.read()))
             df = tdms_file.as_dataframe()
-            return df
+            return None, df
         except Exception:
-            return None
+            return None, None
 
     elif file_name.endswith(".lvm"):
         try:
             return pd.read_csv(file, sep='\t', comment='#')
         except Exception:
-            return None
+            return None, None
 
-    return None
+    return None, None
 
 def detect_anomalies(df):
     numeric_cols = df.select_dtypes(include=[np.number]).columns.tolist()
