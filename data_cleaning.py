42d0883faa52910ae6487b1e7fd4afa5cc27c312
OJH3KOR <shivhariganesh.korde@bosch.com>
2025-05-16 13:23:50 +0530

    data cleaning fix

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,6 +1,17 @@
 import pandas as pd
 
-def clean_data(df: pd.DataFrame, method: str) -> pd.DataFrame:
+def clean_data(df: pd.DataFrame, method: str, column: str = None) -> pd.DataFrame:
+    if column:
+        if method == "Fill with 0":
+            df[column] = df[column].fillna(0)
+        elif method == "Forward Fill":
+            df[column] = df[column].ffill()
+        elif method == "Backward Fill":
+            df[column] = df[column].bfill()
+        elif method == "Drop Nulls":
+            df = df.dropna(subset=[column])
+        return df
+
     if method == "Drop Rows":
         return df.dropna()
     elif method == "Fill with 0":
@@ -10,4 +21,12 @@ def clean_data(df: pd.DataFrame, method: str) -> pd.DataFrame:
     elif method == "Backward Fill":
         return df.bfill()
     else:
-        return df  # default (no change)
+        return df
+
+def auto_clean(df: pd.DataFrame) -> pd.DataFrame:
+    for col in df.columns:
+        if df[col].dtype in ["float64", "int64"]:
+            df[col] = df[col].ffill().fillna(0)
+        else:
+            df[col] = df[col].ffill()
+    return df.dropna()
