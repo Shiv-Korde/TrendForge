e93858be96feb804678ca69818e134a40e9884b8
OJH3KOR <shivhariganesh.korde@bosch.com>
2025-05-16 11:02:09 +0530

    folder fix1

--- data_cleaning.py
+++ data_cleaning.py
@@ -11,16 +11,3 @@ def clean_data(df: pd.DataFrame, method: str) -> pd.DataFrame:
         return df.bfill()
     else:
         return df  # default (no change)
-import pandas as pd
-
-def clean_data(df: pd.DataFrame, method: str) -> pd.DataFrame:
-    if method == "Drop Rows":
-        return df.dropna()
-    elif method == "Fill with 0":
-        return df.fillna(0)
-    elif method == "Forward Fill":
-        return df.ffill()
-    elif method == "Backward Fill":
-        return df.bfill()
-    else:
-        return df  # default (no change)
